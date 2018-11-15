af78b6b050278a5f9e81d6ad82cbb632bb0f5c2a
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2018-11-15 19:10:54 -0200

    Ajustes para mysql

--- src/main/java/com/rar/order/DeliveryOrderServiceApplication.java
+++ src/main/java/com/rar/order/DeliveryOrderServiceApplication.java
@@ -1,11 +1,11 @@
 package com.rar.order;
 
-import org.h2.server.web.WebServlet;
+//import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
+//import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -16,10 +16,10 @@ public class DeliveryOrderServiceApplication {
 		SpringApplication.run(DeliveryOrderServiceApplication.class, args);
 	}
 
-	@Bean
-	public ServletRegistrationBean h2servletRegistration() {
-		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
-		registration.addUrlMappings("/h2/*");
-		return registration;
-	}
+//	@Bean
+//	public ServletRegistrationBean h2servletRegistration() {
+//		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+//		registration.addUrlMappings("/h2/*");
+//		return registration;
+//	}
 }
--- src/main/java/com/rar/order/model/Order.java
+++ src/main/java/com/rar/order/model/Order.java
@@ -17,7 +17,7 @@ public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotEmpty(message = "Product required")
